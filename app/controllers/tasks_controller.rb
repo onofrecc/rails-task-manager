50a329f5bb31a8dfc6796f35a0c59a1941438c5e
Onofre Catala Castellar <onofrecc@gmail.com>
2022-08-16 14:14:49 +0200

    Create implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,24 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def tasks_params
+    # params.require(:restaurant).permit(:name, :address, :rating)
+    params.require(:task).permit(:title, :details)
   end
 end
